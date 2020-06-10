7eb3988d36cd8a0ef4d9fd263f6c7150d78a1abb
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-06-10 16:09:28 +0200

    Mise en place des règles de l'encapsulation

---
+++ src/main/java/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,14 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	
+	private int numCompte;
+	private double solde;
+	
+	public Compte(int numCompte, double solde)
+	{
+		this.numCompte = numCompte;
+		this.solde = solde;
+	}
+
+}
---
+++ src/main/java/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,14 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		
+		Compte c = new Compte(55550000, 123.393);
+		
+		System.out.println(c); 
+		// valeur retournée : "fr.diginamic.banque.entites.Compte@5aaa6d82"
+
+	}
+
+}
