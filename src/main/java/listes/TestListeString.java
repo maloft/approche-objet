3e3a8fa43b21d1a284944ef746e70e56cd2cebb5
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-06-16 16:01:05 +0200

    TP1 Apprendre à utiliser List et ArrayList : Début exercice 4

---
+++ src/main/java/listes/TestListeString.java
@@ -0,0 +1,24 @@
+package listes;
+
+import java.util.ArrayList;
+
+public class TestListeString {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> liste = new ArrayList<>();
+		
+		liste.add("Nice");
+		liste.add("Carcassonne");
+		liste.add("Narbonne");
+		liste.add("Lyon");
+		liste.add("Foix");
+		liste.add("Pau");
+		liste.add("Marseille");
+		liste.add("Tarbes");
+		
+		
+
+	}
+
+}
